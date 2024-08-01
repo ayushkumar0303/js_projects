d09b6c499db1e1341a87d14f9b6b97666059f172
Ayush Kumar <ayush@Ayushs-MacBook-Air.local>
2024-08-01 15:01:25 +0530

    update css and fix draw bug

--- Tic tac toe/script.js
+++ Tic tac toe/script.js
@@ -7,6 +7,7 @@ let turnO = true;
 
 let matchWin=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let cnt=0;
+// let findWinner=false;
 
 let resetButton=()=>{
   winnerStatus.innerText='';
@@ -14,6 +15,8 @@ let resetButton=()=>{
   boxes.forEach((box)=>{
     box.innerHTML='';
     box.disabled=false;
+    box.classList.remove("winner");
+    // console.log(box.classList);
   });
   cnt=0;
 
@@ -24,13 +27,18 @@ function checkWinner(){
     if(boxes[val[0]].innerHTML!=='' && boxes[val[1]].innerHTML!=='' && boxes[val[2]].innerHTML!=='' ){
       if(boxes[val[0]].innerHTML===boxes[val[1]].innerHTML && boxes[val[1]].innerHTML===boxes[val[2]].innerHTML){
         // console.log("Winner is found");
+        boxes[val[0]].classList.add("winner");
+        boxes[val[1]].classList.add("winner");
+        boxes[val[2]].classList.add("winner");
         winnerStatus.innerText=`Congratulations!!! Player ${boxes[val[0]].innerHTML} won the match.`;
         boxes.forEach((box)=>{
           box.disabled=true;
         });
+        return true;
       }
     }
   }
+  return false;
 }
 
 resetBtn.addEventListener("click",resetButton);
@@ -48,8 +56,8 @@ boxes.forEach(box=>{
     }
     cnt++;
     box.disabled=true;
-    checkWinner();
-    if(cnt===9){
+    // checkWinner();
+    if(!checkWinner() && cnt===9){
       winnerStatus.innerText=`Opps!!! It's draw.`;
 
     }
